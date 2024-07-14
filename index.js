36d6677477ee3505aae35b0d2e3b941e01efdb71
Vikas <161747945+VikasIndora@users.noreply.github.com>
2024-07-14 19:27:00 +0530

    Add core 'hover' effect in sketchPad
    Color of auto-generated divs changes to black on event 'mouseenter'

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const sketchPad = document.querySelector('.sketchPad');
+const gridSize = document.querySelectorAll('.menu:nth-child(2)');
+
 
 let num = +prompt(`Enter grid dimension`);
 
 function generateGrid(num) {
-    for (let i = 1; i <= num*num; i++) {
+    for (let i = 1; i <= num * num; i++) {
         const childDiv = document.createElement('div')
-        childDiv.style.cssText = `width: ${479.5/num}px; height: ${479.5/num}px; outline: 1px solid #36454F;`
+        childDiv.style.cssText = `width: ${479.5 / num}px; height: ${479.5 / num}px; outline: 1px solid #36454F;`
+        childDiv.addEventListener('mouseenter', () => {
+            childDiv.className = 'childDivColor';
+        })
         sketchPad.appendChild(childDiv)
     }
 }
