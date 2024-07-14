921c4b85b8537873afae3747cbff1ed4568a4809
Vikas <161747945+VikasIndora@users.noreply.github.com>
2024-07-14 17:30:20 +0530

    Add logic for creating a basic grid
    Fixed height and width of everything for now just to test out the logic

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const sketchPad = document.querySelector('.sketchPad');
-document.createElement('div')
+
+for(let i = 1; i <=64; i++){
+const childDiv = document.createElement('div')
+childDiv.style.cssText = "width: 50px; height: 50px; outline: 1px solid black;"
+sketchPad.appendChild(childDiv)
+}
