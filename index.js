682a4dc437de31096a22b6df1101f30e39c0f891
Vikas <161747945+VikasIndora@users.noreply.github.com>
2024-07-14 20:14:49 +0530

    Fix 'grid size' button to behave as desired
    Now before generating grid it checks for any child in sketchPad container and removes them.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const sketchPad = document.querySelector('.sketchPad');
 const gridSize = document.querySelector('.menu button:nth-child(2)');
 
 function generateGrid(num) {
+    while (sketchPad.firstChild) {
+        sketchPad.removeChild(sketchPad.firstChild);
+    }
     for (let i = 1; i <= num * num; i++) {
         const childDiv = document.createElement('div')
         childDiv.style.cssText = `width: ${479.5 / num}px; height: ${479.5 / num}px; outline: 1px solid #36454F;`
